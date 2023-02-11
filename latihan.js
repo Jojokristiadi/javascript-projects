bcafe4e2fc6bf6dde4ffe59172d0487c227cda3a
Jojokristiadi <joshua.kristiadi@student.umn.ac.id>
2023-02-12 01:27:42 +0700

    node_modules

---
+++ latihan.js
@@ -0,0 +1,9 @@
+// Untuk memudahkan developer dalam proses pengembangan, Node.js 
+// menyediakan beberapa modul bawaan yang dapat Anda
+//  manfaatkan guna mendukung efisiensi untuk melakukan hal-hal yang umum. 
+//  Modul bawaan tersebut dikenal sebagai core modules. 
+//  Anda bisa mengimpor core modules dengan fungsi yang sama, yakni require().
+
+//menginport core module http
+
+const http = require('http')
