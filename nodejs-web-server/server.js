d21ede240b9dcf7f735534c6fdbada0b9a1c48e1
Jojokristiadi <joshua.kristiadi@student.umn.ac.id>
2023-02-13 01:18:45 +0700

    create status code

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -6,33 +6,42 @@ const requestListener = (request, response) => {
 
   const { method, url } = request;
 
-  if(url === '/') {
-    if(method === 'GET') {
-        response.end('<h1>Ini adalah homepage</h1>');
+  if (url === "/") {
+    if (method === "GET") {
+      response.statusCode = 200; //ok
+      response.end("<h1>Ini adalah homepage</h1>");
     } else {
-        response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+      response.statusCode = 400; //bad request
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
     }
-} else if(url === '/about') {
-    if(method === 'GET') {
-        response.end('<h1>Halo! Ini adalah halaman about</h1>')
-    } else if(method === 'POST') {
-        let body = [];
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Halo! Ini adalah halaman about</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
-        });
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.statusCode = 200; //ok
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
     } else {
-        response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+      response.statusCode = 400; //bad request
+      response.end(
+        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+      );
     }
-} else {
-    response.end('<h1>Halaman tidak ditemukan!</h1>');
-}
+  } else {
+    response.statusCode = 404; //not found
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
+  }
 };
 // if (method === "GET") {
 //   response.end("<h1>Hello!</h1>");
